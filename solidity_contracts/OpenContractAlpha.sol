604160546cfbc338d28d3fb43cac8c301c25886a
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2021-10-27 14:47:43 -0700

    Update OpenContractAlpha.sol

--- solidity_contracts/OpenContractAlpha.sol
+++ solidity_contracts/OpenContractAlpha.sol
@@ -2,11 +2,11 @@ pragma solidity >=0.8.0;
 
 
 contract OpenContractAlpha {
-    address hub = 0x9dAe5581fAf4a2C11150D8302D80D4009d2DFDa9;
+    address forwarder = 0x9dAe5581fAf4a2C11150D8302D80D4009d2DFDa9;
 
     modifier onlyOracle(bytes32 oracleHash, bytes32 allowedHash) {
         require(oracleHash == allowedHash, "Incorrect Oracle Hash.");
-        require(msg.sender == hub, "Call has to be relayed by Open Contracts Hub.");
+        require(msg.sender == forwarder, "Call has to be relayed by Open Contracts Hub.");
         _;
     }
     
