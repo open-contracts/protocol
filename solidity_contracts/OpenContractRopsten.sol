db8f6584ab109df9b97d6141781b03f518e8b167
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2021-10-28 13:39:43 -0700

    Update OpenContractRopsten.sol

--- solidity_contracts/OpenContractRopsten.sol
+++ solidity_contracts/OpenContractRopsten.sol
@@ -1,9 +1,8 @@
 pragma solidity >=0.8.0;
 
-
 contract OpenContractAlpha {
     address forwarder = 0x9dAe5581fAf4a2C11150D8302D80D4009d2DFDa9;
-
+    
     modifier onlyOracle(bytes32 oracleHash, bytes32 allowedHash) {
         require(oracleHash == allowedHash, "Incorrect Oracle Hash.");
         require(msg.sender == forwarder, "Call has to be relayed by Open Contracts Hub.");
