cc236d299bcea8660d18b34674b470d11a5764d4
open-contracts <90533597+open-contracts@users.noreply.github.com>
2022-01-09 17:52:02 +0100

    Update OpenContractRopsten.sol

--- solidity_contracts/OpenContractRopsten.sol
+++ solidity_contracts/OpenContractRopsten.sol
@@ -4,11 +4,11 @@ contract OpenContract {
     address private hub = 0xACf12733cBa963201Fdd1757b4D7A062AD096dB1;
     mapping(bytes8 => bytes32) private allowedID;
  
-    function setOracle(bytes8 functionSelector, bytes32 oracleID) internal {
+    function setOracle(bytes8 selector, bytes32 oracleID) internal {
         allowedID[functionSelector] = oracleID;
     }
  
-    modifier checkOracle(bytes32 oracleID, bytes4 selector) {
+    modifier checkOracle(bytes4 selector, bytes32 oracleID) {
         require(msg.sender == hub, "Can only be called via Open Contracts Hub.");
         if (allowedID[selector] != "any") {
             require(oracleID == allowedID[selector], "Incorrect OracleID.");
