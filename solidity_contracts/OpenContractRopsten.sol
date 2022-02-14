7782fda1dd5259a5c89cfddd8e5bcc3c724091a3
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-14 11:27:04 -0800

    Update OpenContractRopsten.sol

--- solidity_contracts/OpenContractRopsten.sol
+++ solidity_contracts/OpenContractRopsten.sol
@@ -3,11 +3,13 @@ pragma solidity >=0.8.0;
 contract OpenContract {
     OpenContractsHub private hub = OpenContractsHub(0x0D75EF6ED06DEE7fA9235a1279B3040D0FDB0217);
  
+    // this call tells the Hub which oracleID is allowed for a given contract function
     function setOracle(bytes4 selector, bytes32 oracleID) internal {
         hub.setOracle(selector, oracleID);
     }
  
     modifier requiresOracle {
+        // the Hub uses the Verifier to ensure that the calldata came from the right oracleID
         require(msg.sender == address(hub), "Can only be called via Open Contracts Hub.");
         _;
     }
