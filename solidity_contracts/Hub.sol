a18d2eb80d8f0c893cf20930aa7a1c46ae137c05
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-14 17:41:35 -0800

    Update Hub.sol

--- solidity_contracts/Hub.sol
+++ solidity_contracts/Hub.sol
@@ -4,7 +4,7 @@ contract OpenContractsHub {
     address public OpenContractsVerifier;
     address public OpenContractsDevs = 0xc3e9591EDB56DcC951D50CD5002108e9d8968410;
     bool public updatable = true;
-    mapping(address => mapping(bytes4 => bytes32)) private ID;
+    mapping(address => mapping(bytes4 => bytes32)) private hash;
 
     // the devs may update the verifier and their address, if the hub is still updateable
     function update(address newVerifier, address newDevAddress, bool stayUpdatable) public {
@@ -15,23 +15,23 @@ contract OpenContractsHub {
         updatable = stayUpdatable;
     }
 
-    // lets an Open Contract to declare which function can be called with which oracleID
-    function setOracleID(bytes4 selector, bytes32 oracleID) public {
-        ID[msg.sender][selector] = oracleID;
+    // lets an Open Contract declare which function can be called with which oracleHash
+    function setOracleHash(bytes4 selector, bytes32 oracleHash) public {
+        hash[msg.sender][selector] = oracleHash;
     }
 
-    // allows anyone to check which oracleID is allowed by a given Open Contract function
-    function getOracleID(address openContract, bytes4 selector) public view returns(bytes32) {
-        return ID[openContract][selector];
+    // allows anyone to check which oracleHash is allowed by a given Open Contract function
+    function getOracleHash(address openContract, bytes4 selector) public view returns(bytes32) {
+        return hash[openContract][selector];
     }
 
-    // forwards call to an Open Contract, if it was validated by the Verifier and produced by the right oracleID
-    function forwardCall(address payable openContract, bytes32 oracleID, bytes memory call) public payable returns(bool, bytes memory) {
+    // forwards call to an Open Contract, if it was validated by the Verifier and produced by the right oracleHash
+    function forwardCall(address payable openContract, bytes32 oracleHash, bytes memory call) public payable returns(bool, bytes memory) {
         require(msg.sender == OpenContractsVerifier, "Only calls from the verifier will be forwarded.");
         bytes4 selector = bytes4(call);
-        bytes32 allowedID = getOracleID(openContract, selector);
-        if (allowedID != bytes32(0)) {     // if no oracleID is set, any oracleID is allowed.
-            require(allowedID == oracleID, "The oracleID is not allowed for this open contract function.");
+        bytes32 allowedHash = getOracleHash(openContract, selector);
+        if (allowedHash != bytes32(0)) {     // if no oracleHash is set, any oracleHash is allowed.
+            require(allowedHash == oracleHash, "Incorrect oracleHash for this open contract function.");
         }
         return openContract.call{value: msg.value, gas: gasleft()}(call);
     }
