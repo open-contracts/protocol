7f31eb07d470cb95807f1ecc12a38b1ed1bbb315
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-27 19:36:09 -0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,27 +1,40 @@
 pragma solidity >=0.8.0;
 
-contract OpenContractsForwarder {
-    address public hub;
-    address public devAddress;
-    bool public completed = false;
+contract OpenContractsHub {
+    address public OpenContractsVerifier;
+    address public OpenContractsDevs = 0xc3e9591EDB56DcC951D50CD5002108e9d8968410;
+    bool public updatable = true;
+    mapping(address => mapping(bytes4 => bytes32)) private hash;
 
-    // upon deployment, we set the addresses of the first hub and of the Open Contracts Devs.
-    constructor() {
-        devAddress = msg.sender;
+    // the devs may update the verifier and their address, if the hub is still updateable
+    function update(address newVerifier, address newDevAddress, bool stayUpdatable) public {
+        require(updatable, "The hub can no longer be updated.");
+        require(msg.sender == OpenContractsDevs, "Only the devs can update the verifier.");
+        OpenContractsVerifier = newVerifier;
+        OpenContractsDevs = newDevAddress;
+        updatable = stayUpdatable;
     }
 
-    // if not frozen, allows the devs to update the hub and their address.
-    function update(address newHub, address newDevAddress, bool complete) public {
-        require(!completed, "The hub can no longer be updated.");
-        require(msg.sender == devAddress, "Only the devs can update the forwarder.");
-        hub = newHub;
-        devAddress = newDevAddress;
-        completed = complete;
+    // lets an Open Contract declare which function can be called with which oracleHash
+    function setOracleHash(bytes4 selector, bytes32 oracleHash) public {
+        hash[msg.sender][selector] = oracleHash;
     }
 
-    // forwards call to destination contract
-    function forwardCall(address payable destinationContract, bytes memory call) public payable returns(bool, bytes memory) {
-        require(msg.sender == hub, 'Only the current hub can use the forwarder.');
-        return destinationContract.call{value: msg.value, gas: gasleft()}(call);
+    // allows anyone to check which oracleHash is allowed by a given Open Contract function
+    function getOracleHash(address openContract, bytes4 selector) public view returns(bytes32) {
+        return hash[openContract][selector];
+    }
+
+    // forwards call to an Open Contract, if it was validated by the Verifier and produced by the right oracleHash
+    function forwardCall(address payable openContract, bytes32 oracleHash, bytes memory call) public payable returns(bool, bytes memory) {
+        require(msg.sender == OpenContractsVerifier, "Only calls from the verifier will be forwarded.");
+        bytes4 selector = bytes4(call);
+        bytes32 allowedHash = getOracleHash(openContract, selector);
+        if (allowedHash != bytes32(0)) {     // if no oracleHash is set, any oracleHash is allowed.
+            require(allowedHash == oracleHash, "Incorrect oracleHash for this open contract function.");
+        }
+        return openContract.call{value: msg.value, gas: gasleft()}(call);
     }
 }
+
+
