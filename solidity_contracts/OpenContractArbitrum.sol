14d5ac1d378b70d45bc2d4e4902020696ac776a6
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-03-10 18:51:25 -0800

    Create OpenContractArbitrum.sol

---
+++ solidity_contracts/OpenContractArbitrum.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.8.0;
+
+contract OpenContract {
+    OpenContractsHub private hub = OpenContractsHub(0x35ea44C6808cb0a490a1E849a0305837F281Aa3B);
+ 
+    // this call tells the Hub which oracleID is allowed for a given contract function
+    function setOracleHash(bytes4 selector, bytes32 oracleHash) internal {
+        hub.setOracleHash(selector, oracleHash);
+    }
+ 
+    modifier requiresOracle {
+        // the Hub uses the Verifier to ensure that the calldata came from the right oracleID
+        require(msg.sender == address(hub), "Can only be called via Open Contracts Hub.");
+        _;
+    }
+}
+
+interface OpenContractsHub {
+    function setOracleHash(bytes4, bytes32) external;
+}
